175ae2f8d78c5f4f4ff7203a69c58c4074ae77f9
Avijit Karmaker <ajkacca@gmail.com>
2021-02-23 12:57:18 +0200

    change of style

--- assets/js/main.js
+++ assets/js/main.js
@@ -170,16 +170,4 @@
     items: 1
   });
 
-  // Init AOS
-  function aos_init() {
-    AOS.init({
-      duration: 1000,
-      easing: "ease-in-out-back",
-      once: true
-    });
-  }
-  $(window).on('load', function() {
-    aos_init();
-  });
-
 })(jQuery);
